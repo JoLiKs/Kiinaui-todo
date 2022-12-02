9482079394388f888e42688e56a83a21f958f10b
Kiinaui <vonkad002@gmail.com>
2022-12-02 20:41:33 +0300

    6 commit Added style for checkbox.

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,15 +2,22 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { ListGroup } from "react-bootstrap";
 import {disableTask} from "../store/actions";
+import cls from "./Task.module.css";
 
 const Task = ({ task }) => {
     const { id, title, completed } = task;
     const dispatch = useDispatch();
-
     return (
-        <ListGroup.Item>
-            <input checked={completed} type="checkbox"/>
-            <span onClick={() => dispatch(disableTask(id))}>{title}</span>
+        <ListGroup.Item className={cls.text}>
+            <label className={cls.label}>
+                <input onClick={() => dispatch(disableTask(id))} className={cls.label__checkbox} type="checkbox"/>
+                <span className={cls.label__text}>
+                    <span className={cls.label__check}>
+                {completed ? <i className="fa fa-check icon"></i> : <span></span>}
+                    </span>
+                </span>
+            </label>
+            {!completed ? <span>{title}</span> : <span className={cls.completed}>{title}</span>}
         </ListGroup.Item>
     )
 }
